dc0cf13b9f63e9beb6e2d63f004d2a973bed6bb6
Dmitriy Zayceff <dmitry.zaycev@esector.ru>
2014-04-15 19:04:50 +0500

    Improve php-capability for __* magic methods

--- jphp-core/src/php/runtime/lang/support/MagicSignatureClass.java
+++ jphp-core/src/php/runtime/lang/support/MagicSignatureClass.java
@@ -4,26 +4,28 @@ import php.runtime.annotation.Reflection;
 import php.runtime.env.Environment;
 import php.runtime.Memory;
 
+import static php.runtime.annotation.Reflection.Arg;
+
 
 @Reflection.Name("Object")
 public class MagicSignatureClass {
 
-    @Reflection.Signature({@Reflection.Arg("property")})
+    @Reflection.Signature({@Arg("property")})
     public Memory __get(Environment env, Memory... args){
         return Memory.NULL;
     }
 
-    @Reflection.Signature({@Reflection.Arg("property"), @Reflection.Arg("value")})
+    @Reflection.Signature({@Arg("property"), @Arg("value")})
     public Memory __set(Environment env, Memory... args){
         return Memory.NULL;
     }
 
-    @Reflection.Signature({@Reflection.Arg("name"), @Reflection.Arg("arguments")})
+    @Reflection.Signature({@Arg("name"), @Arg("arguments")})
     public Memory __call(Environment env, Memory... args){
         return Memory.NULL;
     }
 
-    @Reflection.Signature({@Reflection.Arg("name"), @Reflection.Arg("arguments")})
+    @Reflection.Signature({@Arg("name"), @Arg("arguments")})
     public static Memory __callStatic(Environment env, Memory... args){
         return Memory.NULL;
     }
