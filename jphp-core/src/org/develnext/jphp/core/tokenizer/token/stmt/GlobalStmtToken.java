a8efa9ba471ae47796b38583f2758cfda5e749db
Dmitriy Zayceff <dmitry.zaycev@esector.ru>
2014-04-16 12:49:57 +0500

    Improve php-capability: add support var-var in global statements

--- jphp-core/src/org/develnext/jphp/core/tokenizer/token/stmt/GlobalStmtToken.java
+++ jphp-core/src/org/develnext/jphp/core/tokenizer/token/stmt/GlobalStmtToken.java
@@ -1,24 +1,25 @@
 package org.develnext.jphp.core.tokenizer.token.stmt;
 
-import org.develnext.jphp.core.tokenizer.TokenType;
 import org.develnext.jphp.core.tokenizer.TokenMeta;
-import org.develnext.jphp.core.tokenizer.token.expr.value.VariableExprToken;
+import org.develnext.jphp.core.tokenizer.TokenType;
+import org.develnext.jphp.core.tokenizer.token.expr.ValueExprToken;
 
 import java.util.List;
 
 public class GlobalStmtToken extends StmtToken {
 
-    private List<VariableExprToken> variables;
+    // list of vars or var-vars
+    private List<ValueExprToken> variables;
 
     public GlobalStmtToken(TokenMeta meta) {
         super(meta, TokenType.T_GLOBAL);
     }
 
-    public List<VariableExprToken> getVariables() {
+    public List<ValueExprToken> getVariables() {
         return variables;
     }
 
-    public void setVariables(List<VariableExprToken> variables) {
+    public void setVariables(List<ValueExprToken> variables) {
         this.variables = variables;
     }
 }
--- jphp-core/tests/resources/variables/global.php
+++ jphp-core/tests/resources/variables/global.php
@@ -9,4 +9,12 @@ function test(){
     return $x . $y . "|" . $GLOBALS['x'] . $GLOBALS['y'];
 }
 
+function test2($var) {
+    global ${$var}, $y;
+    return ${$var} . $y;
+}
+
+if (test2('x') !== 'foobar')
+    return 'fail_2: var-var';
+
 return test();
