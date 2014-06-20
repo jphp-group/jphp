04469684e2b7f9e637eafbd54274f2106292c28b
Dmitriy Zayceff <dz@dim-s.net>
2014-06-20 18:33:00 +0500

    [http-ext] Add support for client config

--- jphp-http-ext/sdk/php/net/HttpClient.php
+++ jphp-http-ext/sdk/php/net/HttpClient.php
@@ -3,6 +3,9 @@ namespace php\net;
 
 /**
  * Class HttpClient
+ *
+ *  Cloneable
+ *
  * @package php\net
  */
 class HttpClient {
@@ -14,8 +17,41 @@ class HttpClient {
 
     /**
      * @param array $headers
+     * @return HttpClient
      */
-    public function setHeaders(array $headers) { }
+    public function setHeaders(array $headers) { return $this; }
+
+    /**
+     * @param int $value - in milliseconds
+     * @return HttpClient
+     */
+    public function setConnectTimeout($value) { return $this; }
+
+    /**
+     * @param int $value - in milliseconds
+     * @return HttpClient
+     */
+    public function setSocketTimeout($value) { return $this; }
+
+    /**
+     * @param int $value -1 means that no limits
+     * @return HttpClient
+     */
+    public function setMaxRedirects($value) { return $this; }
+
+    /**
+     * @param bool $value
+     * @return HttpClient
+     */
+    public function setRedirectsEnabled($value) { return $this; }
+
+    /**
+     * @param string $hostname
+     * @param int $port
+     * @param string $scheme
+     * @return HttpClient
+     */
+    public function setProxy($hostname, $port = -1, $scheme = 'http') { return $this; }
 
     /**
      * @param $url
@@ -40,5 +76,23 @@ class HttpClient {
      * @return HttpRequest
      */
     public function delete($url) { return new HttpRequest(); }
+
+    /**
+     * @param $url
+     * @return HttpRequest
+     */
+    public function head($url) { return new HttpRequest(); }
+
+    /**
+     * @param $url
+     * @return HttpRequest
+     */
+    public function patch($url) { return new HttpRequest(); }
+
+    /**
+     * @param $url
+     * @return HttpRequest
+     */
+    public function options($url) { return new HttpRequest(); }
 }
 
--- jphp-http-ext/src/main/java/org/develnext/jphp/http/classes/WrapHttpClient.java
+++ jphp-http-ext/src/main/java/org/develnext/jphp/http/classes/WrapHttpClient.java
@@ -1,11 +1,15 @@
 package org.develnext.jphp.http.classes;
 
+import org.apache.http.HttpHost;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.*;
 import php.runtime.Memory;
 import php.runtime.common.HintType;
 import php.runtime.env.Environment;
+import php.runtime.env.TraceInfo;
 import php.runtime.lang.BaseObject;
 import php.runtime.lang.ForeachIterator;
+import php.runtime.lang.support.ICloneableObject;
 import php.runtime.memory.ArrayMemory;
 import php.runtime.memory.ObjectMemory;
 import php.runtime.reflection.ClassEntity;
@@ -16,7 +20,8 @@ import java.util.Map;
 import static php.runtime.annotation.Reflection.*;
 
 @Name("php\\net\\HttpClient")
-public class WrapHttpClient extends BaseObject {
+public class WrapHttpClient extends BaseObject implements ICloneableObject<WrapHttpClient> {
+    protected RequestConfig.Builder configBuilder = RequestConfig.copy(RequestConfig.DEFAULT);
     protected Map<String, String> headers = new LinkedHashMap<String, String>();
 
     public WrapHttpClient(Environment env) {
@@ -33,7 +38,7 @@ public class WrapHttpClient extends BaseObject {
                 httpRequestBase.setHeader(el.getKey(), el.getValue());
             }
         }
-
+        httpRequestBase.setConfig(configBuilder.build());
         return httpRequestBase;
     }
 
@@ -50,7 +55,41 @@ public class WrapHttpClient extends BaseObject {
             headers.put(iterator.getMemoryKey().toString(), iterator.getValue().toString());
         }
 
-        return Memory.NULL;
+        return new ObjectMemory(this);
+    }
+
+    @Signature(@Arg("millis"))
+    public Memory setConnectTimeout(Environment env, Memory... args) {
+        configBuilder.setConnectTimeout(args[0].toInteger());
+        return new ObjectMemory(this);
+    }
+
+    @Signature(@Arg("value"))
+    public Memory setMaxRedirects(Environment env, Memory... args) {
+        configBuilder.setMaxRedirects(args[0].toInteger());
+        return new ObjectMemory(this);
+    }
+
+    @Signature(@Arg("value"))
+    public Memory setSocketTimeout(Environment env, Memory... args) {
+        configBuilder.setSocketTimeout(args[0].toInteger());
+        return new ObjectMemory(this);
+    }
+
+    @Signature(@Arg("value"))
+    public Memory setRedirectsEnabled(Environment env, Memory... args) {
+        configBuilder.setRedirectsEnabled(args[0].toBoolean());
+        return new ObjectMemory(this);
+    }
+
+    @Signature({
+            @Arg("hostname"),
+            @Arg(value = "port", optional = @Optional("-1")),
+            @Arg(value = "scheme", optional = @Optional("http"))
+    })
+    public Memory setProxy(Environment env, Memory... args) {
+        configBuilder.setProxy(new HttpHost(args[0].toString(), args[1].toInteger(), args[2].toString()));
+        return new ObjectMemory(this);
     }
 
     @Signature(@Arg("url"))
@@ -80,8 +119,42 @@ public class WrapHttpClient extends BaseObject {
     @Signature(@Arg("url"))
     public Memory delete(Environment env, Memory... args) {
         WrapHttpRequest request = new WrapHttpRequest(env, this);
-        request.setHttpRequest(makeHttpRequest(new HttpDelete(args[0].toString())));
 
+        request.setHttpRequest(makeHttpRequest(new HttpDelete(args[0].toString())));
         return new ObjectMemory(request);
+    }
+
+    @Signature(@Arg("url"))
+    public Memory head(Environment env, Memory... args) {
+        WrapHttpRequest request = new WrapHttpRequest(env, this);
+
+        request.setHttpRequest(makeHttpRequest(new HttpHead(args[0].toString())));
+        return new ObjectMemory(request);
+    }
+
+    @Signature(@Arg("url"))
+    public Memory patch(Environment env, Memory... args) {
+        WrapHttpRequest request = new WrapHttpRequest(env, this);
+
+        request.setHttpRequest(makeHttpRequest(new HttpPatch(args[0].toString())));
+        return new ObjectMemory(request);
+    }
+
+
+    @Signature(@Arg("url"))
+    public Memory options(Environment env, Memory... args) {
+        WrapHttpRequest request = new WrapHttpRequest(env, this);
+
+        request.setHttpRequest(makeHttpRequest(new HttpOptions(args[0].toString())));
+        return new ObjectMemory(request);
+    }
+
+    @Override
+    public WrapHttpClient __clone(Environment env, TraceInfo trace) {
+        WrapHttpClient r = new WrapHttpClient(env);
+        r.configBuilder = RequestConfig.copy(configBuilder.build());
+        r.headers = new LinkedHashMap<String, String>(headers);
+
+        return r;
     }
 }
