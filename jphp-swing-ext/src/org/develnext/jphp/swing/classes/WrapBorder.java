e63e7ce06711397c83b5bf0415034f7c58dec98b
Dmitriy Zayceff <dz@dim-s.net>
2014-07-17 18:32:09 +0500

    [swing-ext] Improve Border class

--- jphp-swing-ext/src/org/develnext/jphp/swing/classes/WrapBorder.java
+++ jphp-swing-ext/src/org/develnext/jphp/swing/classes/WrapBorder.java
@@ -1,14 +1,16 @@
 package org.develnext.jphp.swing.classes;
 
-import php.runtime.Memory;
-import php.runtime.env.Environment;
 import org.develnext.jphp.swing.SwingExtension;
 import org.develnext.jphp.swing.classes.components.support.RootObject;
+import php.runtime.Memory;
+import php.runtime.env.Environment;
 import php.runtime.memory.ObjectMemory;
 import php.runtime.reflection.ClassEntity;
 
-import javax.swing.border.Border;
-import javax.swing.border.EmptyBorder;
+import javax.swing.*;
+import javax.swing.border.*;
+
+import java.awt.*;
 
 import static php.runtime.annotation.Reflection.*;
 
@@ -29,11 +31,112 @@ public class WrapBorder extends RootObject {
         return border;
     }
 
+    @Signature
+    public Memory isOpaque(Environment env, Memory... args) {
+        return border.isBorderOpaque() ? Memory.TRUE : Memory.FALSE;
+    }
+
     @Signature({@Arg("top"), @Arg("left"), @Arg("bottom"), @Arg("right")})
     public static Memory createEmpty(Environment env, Memory... args) {
         return new ObjectMemory(new WrapBorder(env, new EmptyBorder(
             args[0].toInteger(), args[1].toInteger(),
             args[2].toInteger(), args[3].toInteger()
         )));
+    }
+
+    @Signature({
+            @Arg("type"),
+            @Arg("highlightColor"),
+            @Arg("shadowColor")
+    })
+    public static Memory createBevel(Environment env, Memory... args) {
+        try {
+            return new ObjectMemory(new WrapBorder(env, BorderFactory.createBevelBorder(
+                    BevelBorder.class.getField(args[0].toString().toUpperCase()).getInt(null),
+                    WrapColor.of(args[1]),
+                    WrapColor.of(args[2])
+            )));
+        } catch (IllegalAccessException e) {
+            throw new IllegalArgumentException(e);
+        } catch (NoSuchFieldException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    @Signature({
+            @Arg("title"),
+            @Arg(value = "border", nativeType = WrapBorder.class, optional = @Optional("null")),
+            @Arg(value = "titleFont", nativeType = WrapFont.class, optional = @Optional("null")),
+            @Arg(value = "titleColor", optional = @Optional("null"))
+    })
+    public static Memory createTitled(Environment env, Memory... args) {
+        Border parent = null;
+        if (!args[1].isNull())
+            parent = args[1].toObject(WrapBorder.class).getBorder();
+
+        Font font = null;
+        if (!args[2].isNull())
+            font = args[2].toObject(WrapFont.class).getFont();
+
+        Color color = null;
+        if (!args[3].isNull())
+            color = WrapColor.of(args[3]);
+
+        return new ObjectMemory(new WrapBorder(env, BorderFactory.createTitledBorder(
+                parent,
+                args[0].toString(),
+                TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION,
+                font,
+                color
+        )));
+    }
+
+    @Signature({
+            @Arg("color"),
+            @Arg(value = "size", optional = @Optional("1")),
+            @Arg(value = "rounded", optional = @Optional("false"))
+    })
+    public static Memory createLine(Environment env, Memory... args) {
+        return new ObjectMemory(new WrapBorder(env, BorderFactory.createLineBorder(
+                WrapColor.of(args[0]), args[1].toInteger(), args[2].toBoolean()
+        )));
+    }
+
+    @Signature({
+            @Arg("type"),
+            @Arg("highlightColor"),
+            @Arg("shadowColor")
+    })
+    public static Memory createSoftBevel(Environment env, Memory... args) {
+        try {
+            return new ObjectMemory(new WrapBorder(env, BorderFactory.createSoftBevelBorder(
+                    BevelBorder.class.getField(args[0].toString().toUpperCase()).getInt(null),
+                    WrapColor.of(args[1]),
+                    WrapColor.of(args[2])
+            )));
+        } catch (IllegalAccessException e) {
+            throw new IllegalArgumentException(e);
+        } catch (NoSuchFieldException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    @Signature({
+            @Arg("type"),
+            @Arg("highlightColor"),
+            @Arg("shadowColor")
+    })
+    public static Memory createEtchedBevel(Environment env, Memory... args) {
+        try {
+            return new ObjectMemory(new WrapBorder(env, BorderFactory.createEtchedBorder(
+                    EtchedBorder.class.getField(args[0].toString().toUpperCase()).getInt(null),
+                    WrapColor.of(args[1]),
+                    WrapColor.of(args[2])
+            )));
+        } catch (IllegalAccessException e) {
+            throw new IllegalArgumentException(e);
+        } catch (NoSuchFieldException e) {
+            throw new IllegalArgumentException(e);
+        }
     }
 }
