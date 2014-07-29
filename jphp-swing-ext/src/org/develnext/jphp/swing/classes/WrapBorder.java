1d8c695ab9606775920ffa6e7a66dd705c441f49
Dmitriy Zayceff <dz@dim-s.net>
2014-07-29 10:52:30 +0500

    Fix compatibility with jdk 1.6

--- jphp-swing-ext/src/org/develnext/jphp/swing/classes/WrapBorder.java
+++ jphp-swing-ext/src/org/develnext/jphp/swing/classes/WrapBorder.java
@@ -11,6 +11,8 @@ import javax.swing.*;
 import javax.swing.border.*;
 
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import java.beans.ConstructorProperties;
 
 import static php.runtime.annotation.Reflection.*;
 
@@ -159,7 +161,7 @@ public class WrapBorder extends RootObject {
         int cap = rounded ? BasicStroke.CAP_ROUND : BasicStroke.CAP_SQUARE;
         int join = rounded ? BasicStroke.JOIN_ROUND : BasicStroke.JOIN_MITER;
         float[] array = { thickness * (length - 1.0f), thickness * (spacing + 1.0f) };
-        Border border = BorderFactory.createStrokeBorder(
+        Border border = new StrokeBorder(
                 new BasicStroke(thickness, cap, join, thickness * 2.0f, array, 0.0f), paint
         );
 
@@ -195,5 +197,56 @@ public class WrapBorder extends RootObject {
                 args[0].toObject(WrapBorder.class).getBorder(),
                 args[1].toObject(WrapBorder.class).getBorder()
         )));
+    }
+
+    private static class StrokeBorder extends AbstractBorder {
+        private final BasicStroke stroke;
+        private final Paint paint;
+
+
+        public StrokeBorder(BasicStroke stroke) {
+            this(stroke, null);
+        }
+
+        @ConstructorProperties({ "stroke", "paint" })
+        public StrokeBorder(BasicStroke stroke, Paint paint) {
+            if (stroke == null) {
+                throw new NullPointerException("border's stroke");
+            }
+            this.stroke = stroke;
+            this.paint = paint;
+        }
+
+        @Override
+        public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+            float size = this.stroke.getLineWidth();
+            if (size > 0.0f) {
+                g = g.create();
+                if (g instanceof Graphics2D) {
+                    Graphics2D g2d = (Graphics2D) g;
+                    g2d.setStroke(this.stroke);
+                    g2d.setPaint(this.paint != null ? this.paint : c == null ? null : c.getForeground());
+                    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                            RenderingHints.VALUE_ANTIALIAS_ON);
+                    g2d.draw(new Rectangle2D.Float(x + size / 2, y + size / 2, width - size, height - size));
+                }
+                g.dispose();
+            }
+        }
+
+        @Override
+        public Insets getBorderInsets(Component c, Insets insets) {
+            int size = (int) Math.ceil(this.stroke.getLineWidth());
+            insets.set(size, size, size, size);
+            return insets;
+        }
+
+        public BasicStroke getStroke() {
+            return this.stroke;
+        }
+
+        public Paint getPaint() {
+            return this.paint;
+        }
     }
 }
