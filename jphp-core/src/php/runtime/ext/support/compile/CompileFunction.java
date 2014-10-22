de2a75001275f2ff83bef92e64e76e26de003a24
Dmitriy Zaytsev <d.zayceff@gmail.com>
2014-10-22 15:37:21 +0400

    Improve internal api

--- jphp-core/src/php/runtime/ext/support/compile/CompileFunction.java
+++ jphp-core/src/php/runtime/ext/support/compile/CompileFunction.java
@@ -2,13 +2,17 @@ package php.runtime.ext.support.compile;
 
 
 import php.runtime.annotation.Runtime;
+import php.runtime.common.Messages;
 import php.runtime.env.Environment;
 import php.runtime.env.TraceInfo;
+import php.runtime.exceptions.CriticalException;
+import php.runtime.exceptions.support.ErrorType;
 import php.runtime.memory.ArrayMemory;
 import php.runtime.Memory;
 import php.runtime.memory.support.MemoryUtils;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
 
 public class CompileFunction {
     public final String name;
@@ -213,6 +217,57 @@ public class CompileFunction {
 
         public boolean isVarArg(){
             return method.isVarArgs();
+        }
+
+        public Memory call(Environment env, Memory... arguments) {
+            Class<?>[] types = parameterTypes;
+            Object[] passed = new Object[ types.length ];
+
+            int i = 0;
+            int j = 0;
+            for(Class<?> clazz : types) {
+                boolean isRef = references[i];
+                boolean mutableValue = mutableValues[i];
+
+                MemoryUtils.Converter<?> converter = converters[i];
+                if (clazz == Memory.class) {
+                    passed[i] = isRef ? arguments[j] : (mutableValue ? arguments[j].toImmutable() : arguments[j].toValue());
+                    j++;
+                } else if (converter != null) {
+                    passed[i] = converter.run(arguments[j]);
+                    j++;
+                } else if (clazz == Environment.class) {
+                    passed[i] = env;
+                } else if (clazz == TraceInfo.class) {
+                    passed[i] = env.trace();
+                } else if (i == types.length - 1 && types[i] == Memory[].class){
+                    Memory[] arg = new Memory[arguments.length - i + 1];
+                    if (!isRef){
+                        for(int k = 0; k < arg.length; k++)
+                            arg[i] = arguments[i].toImmutable();
+                    } else {
+                        System.arraycopy(arguments, j, arg, 0, arg.length);
+                    }
+                    passed[i] = arg;
+                    break;
+                } else {
+                    env.error(env.trace(), ErrorType.E_CORE_ERROR, "Cannot call this method dynamically");
+                    passed[i] = Memory.NULL;
+                }
+                i++;
+            }
+
+            try {
+                if (resultType == void.class){
+                    method.invoke(null, passed);
+                    return Memory.NULL;
+                } else
+                    return MemoryUtils.valueOf(method.invoke(null, passed));
+            } catch (InvocationTargetException e){
+                return env.__throwException(e);
+            } catch (IllegalAccessException e) {
+                throw new CriticalException(e);
+            }
         }
     }
 }
