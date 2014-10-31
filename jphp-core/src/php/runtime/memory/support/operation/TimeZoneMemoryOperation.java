b822136e1746092f1a49b98e5ecba7418bd7c8d3
unknown <dz@dim-s.net>
2014-10-31 21:28:43 +0300

    Fix tests

--- jphp-core/src/php/runtime/memory/support/operation/TimeZoneMemoryOperation.java
+++ jphp-core/src/php/runtime/memory/support/operation/TimeZoneMemoryOperation.java
@@ -3,13 +3,11 @@ package php.runtime.memory.support.operation;
 import php.runtime.Memory;
 import php.runtime.env.Environment;
 import php.runtime.env.TraceInfo;
-import php.runtime.ext.core.classes.time.WrapTime;
 import php.runtime.ext.core.classes.time.WrapTimeZone;
 import php.runtime.memory.ObjectMemory;
 import php.runtime.memory.support.MemoryOperation;
 import php.runtime.reflection.ParameterEntity;
 
-import java.util.Date;
 import java.util.TimeZone;
 
 public class TimeZoneMemoryOperation extends MemoryOperation<TimeZone> {
