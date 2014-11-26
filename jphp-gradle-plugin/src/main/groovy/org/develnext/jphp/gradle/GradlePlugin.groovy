70b1289136bd57a0b180c7a09f970c0b6e58d9ab
Dmitriy Zaytsev <d.zayceff@gmail.com>
2014-11-26 17:46:27 +0300

    Update gradle plugin

--- jphp-gradle-plugin/src/main/groovy/org/develnext/jphp/gradle/GradlePlugin.groovy
+++ jphp-gradle-plugin/src/main/groovy/org/develnext/jphp/gradle/GradlePlugin.groovy
@@ -4,9 +4,17 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class GradlePlugin implements Plugin<Project> {
-
     @Override
     void apply(Project project) {
-        println "Hello world!";
+        def sourceSets = project.container(SourceSet);
+
+        def phpSourceSet = sourceSets.find {
+            it.name == "php"
+        }
+
+        if (phpSourceSet == null) {
+            phpSourceSet = new SourceSet("php");
+            phpSourceSet.srcDir = project.file('src/main/php');
+        }
     }
 }
---
+++ jphp-gradle-plugin/src/main/groovy/org/develnext/jphp/gradle/SourceSet.groovy
@@ -0,0 +1,10 @@
+package org.develnext.jphp.gradle
+
+class SourceSet {
+    final String name;
+    File srcDir;
+
+    SourceSet(String name) {
+        this.name = name
+    }
+}
