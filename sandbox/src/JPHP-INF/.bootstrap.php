afa928030b63f716e4d579fc3cbeafbf2ace760b
unknown <dz@dim-s.net>
2015-04-16 23:57:58 +0300

    Fix sandbox, update docs, update version.

--- sandbox/src/JPHP-INF/.bootstrap.php
+++ sandbox/src/JPHP-INF/.bootstrap.php
@@ -1,15 +1,3 @@
 <?php
 
-use php\mail\Email;
-use php\mail\EmailBackend;
-
-$backend = new EmailBackend();
-$backend->setAuthentication('bot@dim-s.net', 'myfghjfghj1');
-
-$backend->hostName     = 'smtp.gmail.com';
-$backend->sslSmtpPort  = 465;
-$backend->sslOnConnect = true;
-
-$email = new Email();
-$email->setTo(['dz@dim-s.net', 'd.zayceff@gmail.com']);
-$email->send($backend);
+echo "Hello World!";
