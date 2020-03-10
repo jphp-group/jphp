9f5be841f10982fcb8b68f9a2529663c59c00fa3
Dmitriy Zayceff <d.zayceff@gmail.com>
2020-03-10 22:50:09 +0300

    Bugfixes.

--- jphp-runtime/src/php/runtime/lang/support/MagicSignatureClass.java
+++ jphp-runtime/src/php/runtime/lang/support/MagicSignatureClass.java
@@ -2,30 +2,29 @@ package php.runtime.lang.support;
 
 import php.runtime.Memory;
 import php.runtime.annotation.Reflection;
+import php.runtime.common.HintType;
 import php.runtime.env.Environment;
 
 import static php.runtime.annotation.Reflection.Arg;
 
-
 @Reflection.Name("Object")
 public class MagicSignatureClass {
-
-    @Reflection.Signature({@Arg("property")})
+    @Reflection.Signature({@Arg(value = "property", type = HintType.STRING)})
     public Memory __get(Environment env, Memory... args){
         return Memory.NULL;
     }
 
-    @Reflection.Signature({@Arg("property"), @Arg("value")})
+    @Reflection.Signature({@Arg(value = "property", type = HintType.STRING), @Arg("value")})
     public Memory __set(Environment env, Memory... args){
         return Memory.NULL;
     }
 
-    @Reflection.Signature({@Arg("name"), @Arg("arguments")})
+    @Reflection.Signature({@Arg(value = "name", type = HintType.STRING), @Arg(value = "arguments", type = HintType.ARRAY)})
     public Memory __call(Environment env, Memory... args){
         return Memory.NULL;
     }
 
-    @Reflection.Signature({@Arg("name"), @Arg("arguments")})
+    @Reflection.Signature({@Arg(value = "name", type = HintType.STRING), @Arg(value = "arguments", type = HintType.ARRAY)})
     public static Memory __callStatic(Environment env, Memory... args){
         return Memory.NULL;
     }
